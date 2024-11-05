06911e739bad63fa8bd98c1f5ab7aef5292485be
yukakeren <ataaqiela.kamil@gmail.com>
2024-11-05 13:13:59 +0700

    .

---
+++ magang6/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/yukakeren/magang6/src
+# Build directory: /home/yukakeren/magang6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arm_controller")
+subdirs("robot_description")
---
+++ magang6/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/yukakeren/magang6/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/yukakeren/magang6/devel;/opt/ros/noetic'.split(';') if '/home/yukakeren/magang6/devel;/opt/ros/noetic' != '' else []
