897dcbbb1dfe58e7703fba8b693d38190db0a053
yukakeren <ataaqiela.kamil@gmail.com>
2024-11-05 13:00:15 +0700

    .

---
+++ magang6/build/arm_controller/CMakeFiles/arm_controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/yukakeren/magang6/devel/lib/arm_controller/arm_controller"
+  "/home/yukakeren/magang6/devel/lib/arm_controller/arm_controller.pdb"
+  "CMakeFiles/arm_controller.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arm_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ magang6/build/arm_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yukakeren/magang6/src/arm_controller
+# Build directory: /home/yukakeren/magang6/build/arm_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
